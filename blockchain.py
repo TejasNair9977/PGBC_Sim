e65c942ec8e5b9fb68a36e28ba6151671f14ac3f
Tejas Nair <tejasnair2002@gmail.com>
2023-04-04 16:43:35 +0530

    changed basic blockchain model, network fix, and modified gitignore and requirements to be more accurate

--- blockchain.py
+++ blockchain.py
@@ -1,96 +1,53 @@
-import json
+import datetime
 import hashlib
-import sqlite3
-from datetime import time, date, datetime
-from cryptography import x509,fernet
+import json
+
 
 class Blockchain:
-
-    def __init__(self):
-        super(Blockchain, self).__init__()
-        self.db = sqlite3.connect('blockchain.db')
-        self.init_database()
-        
-    def init_database(self):
-        c = self.db.cursor()
-        c.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='blockchain'")
-        if ( c.fetchone()[0] != 1 ):
-            c.execute("""CREATE TABLE blockchain(
-                       id INTEGER PRIMARY KEY AUTOINCREMENT,
-                       prev_hash TEXT,
-                       type TEXT,
-                       timestamp TEXT,
-                       data TEXT,
-                       nonce TEXT, 
-                       hash TEXT)""")
-
-    def check_block(self, block):
-        return True
-            
-    def add_block(self, block):
-        if ( self.check_block(block) ):
-            c = self.db.cursor()
-            c.execute("INSERT INTO blockchain (prev_hash, type, timestamp, data, nonce, hash) VALUES (?, ?, ?, ?, ?, ?)",
-                      ( block["prev_hash"],
-                        block["type"], 
-                        block["timestamp"],
-                        json.dumps(block["data"], sort_keys=True),
-                        block["nonce"],
-                        block["hash"] ))
-            self.db.commit()
-            return True
-
-        return False
-
-    def get_blockchain_record(self, data):
-        header = ("id", "prev_hash", "type", "timestamp", "data", "nonce", "hash")
-        
-        if ( len(data) != len(header) ):
-            print("Blockchain data does not contain " + len(header) + " elements")
-            return None
-
-        record = {}
-        for i in range(len(header)):
-            record[header[i]] = data[i]
-            
-        return record
-        
-    def get_block(self, index):
-        c = self.db.cursor()
-        c.execute("SELECT * FROM blockchain WHERE id=?", (index,))
-
-        data = c.fetchone()
-        if ( data != None ):
-            return self.get_blockchain_record(data)
-
-        return None
-            
-    def get_last_block(self):
-        c = self.db.cursor()
-        for row in c.execute('SELECT * FROM blockchain ORDER BY id DESC LIMIT 1'):
-            return self.get_blockchain_record(row)
-        return None
-
-    def process_block(self, data, type):
-        last_block = self.get_last_block()
-        print("LAST:")
-        print(last_block)
-        timestamp = datetime.now()
-        block = {
-            "id"       : (last_block["id"] + 1) if last_block != None else 1,
-            "prev_hash": last_block["hash"] if last_block != None else 0,
-            "type"     : type,
-            "timestamp": timestamp.isoformat(),
-            "data"     : data,
-            "nonce"    : 0
-        }
-        difficulty = 5
-        h = hashlib.sha512()
-        h.update( json.dumps(block, sort_keys=True).encode("utf-8") )
-        while ( h.hexdigest()[:difficulty] != "0"*difficulty ):
-            block["nonce"] = block["nonce"] + 1
-            h.update( json.dumps(block, sort_keys=True).encode("utf-8") )
-
-        block["hash"] = h.hexdigest()
-
-        return block
+	def __init__(self): #genesis block
+		self.chain = []
+		self.create_block(proof=1, previous_hash='0')
+		
+	def create_block(self, proof, previous_hash):
+		block = {'index': len(self.chain) + 1,
+				'timestamp': str(datetime.datetime.now()),
+				'proof': proof,
+				'previous_hash': previous_hash}
+		self.chain.append(block)
+		return block
+	
+	def print_previous_block(self):
+		return self.chain[-1]
+	
+	def proof_of_work(self, previous_proof):
+		new_proof = 1
+		check_proof = False
+		while check_proof is False:
+			hash_operation = hashlib.sha256(
+				str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+			if hash_operation[:5] == '00000':
+				check_proof = True
+			else:
+				new_proof += 1
+		return new_proof
+	
+	def hash(self, block):
+		encoded_block = json.dumps(block, sort_keys=True).encode()
+		return hashlib.sha256(encoded_block).hexdigest()
+	
+	def chain_valid(self, chain):
+		previous_block = chain[0]
+		block_index = 1
+		while block_index < len(chain):
+			block = chain[block_index]
+			if block['previous_hash'] != self.hash(previous_block):
+				return False
+			previous_proof = previous_block['proof']
+			proof = block['proof']
+			hash_operation = hashlib.sha256(
+				str(proof**2 - previous_proof**2).encode()).hexdigest()
+			if hash_operation[:5] != '00000':
+				return False
+			previous_block = block
+			block_index += 1
+		return True
