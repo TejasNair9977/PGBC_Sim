bbe7388fe5c34b9f603e0ad3207a6fce9869f6a0
Gaurav <1032201077@tcetmumbai.in>
2023-04-13 14:52:53 +0530

    Added Graph & Fixed login

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,48 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import searchpng from '../../assets/images/search.png';
-import tejas from '../../assets/images/tejas.png';
-import './index.scss';
-import axios from 'axios';
+import React from 'react'
+import { useState } from 'react'
+import searchpng from '../../assets/images/search.png'
+import ViewInArIcon from '@mui/icons-material/ViewInAr';
+import './index.scss'
 
 const Navbar = () => {
-  const [lastFiveBlocks, setLastFiveBlocks] = useState([]);
-  const [lastEight, setLastEight] = useState('');
-  const [username, setUsername] = useState('');
+    const [search, setSearch] = useState("")
+    
 
-  useEffect(() => {
-    axios.get('/query').then((response) => {
-      setLastFiveBlocks(response.data);
-    });
-
-    axios.get('/last-eight').then((response) => {
-      setLastEight(response.data.last_eight);
-    });
-
-    const storedUsername = localStorage.getItem('username');
-    if (storedUsername) {
-      setUsername(storedUsername);
-    }
-  }, []);
-
-  return (
-    <div className="nav-container">
-      <form className='search-bar' onSubmit={handleSearch}>
-        <input type='text' value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Search..." />
-        <button type='submit'>
+    return (
+      <div className='nav-container'>
+        <div className='search-bar'>
+          <input type='text' value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Search..." />    {/* get */}
           <img id='search' src={searchpng} alt="search icon" />
-        </button>
-      </form>
-      <div id="blackbox">
-        <img src={tejas} alt='Profile pic' />  {/* get */}
-        <span>{username} #{lastEight}</span>
-        <ul id="3-dot-menu">
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
+        </div>
+        <div id='blackbox'>
+          <div className='.icon'>
+              <ViewInArIcon style={{fontSize: '30px', color:'#DCDCDC', marginRight:'3px'}} />
+          </div>        
+          <span>192.168.1.127</span>  {/* get */}
+        </div>
       </div>
-    </div>
-  );
-};
-
-export default Navbar;
+    )
+}
+export default Navbar
